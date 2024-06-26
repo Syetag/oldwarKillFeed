2e6285159903b75e47fe716d2d0fedae08933122
Syetag <109528894+Syetag@users.noreply.github.com>
2024-06-26 23:31:11 +0500

    Add files via upload

---
+++ oldwarKillFeed/Config.cs
@@ -0,0 +1,21 @@
+﻿using Rocket.API;
+
+namespace oldwar
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public ushort EffectID { get; set; }
+        public string PlayerNameColor { get; set; }
+        public short MinEffectKey { get; set; }
+        public short MaxEffectKey { get; set; }
+
+        public void LoadDefaults()
+        {
+            EffectID = 14018;
+            PlayerNameColor = "FFD700";
+
+            MinEffectKey = 0;
+            MaxEffectKey = 150;
+        }
+    }
+}
